b4e99a690f51c6d6bb8f4a25c62e0c97b45dd0e0
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-14 13:57:57 +0530

    Find Element In An Array

---
+++ OneDArray/Q2.java
@@ -0,0 +1,33 @@
+package OneDArray;
+
+import java.util.Scanner;
+
+public class Q2 {
+	
+	private static int getElement(int array[], int d) {
+		
+		for(int i=0;i<array.length;i++) {
+			if(array[i] == d) {
+				return i;
+			}
+		}
+				
+		return -1;
+	}
+	
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int array[] = new int[n];
+		
+		for(int i=0;i<array.length;i++) {
+			array[i] = scn.nextInt();
+		}
+		
+		int d = scn.nextInt();
+		
+		int ans = getElement(array, d);
+		System.out.println(ans);
+	}
+
+}
