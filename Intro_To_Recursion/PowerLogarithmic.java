68cf63e90a5cdbfd04519d2ca7c558d7e3e49e50
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-17 23:46:33 +0530

    Power-logarithmic Using Recursion

---
+++ Intro_To_Recursion/PowerLogarithmic.java
@@ -0,0 +1,34 @@
+package Intro_To_Recursion;
+
+import java.util.Scanner;
+
+public class PowerLogarithmic {
+	
+	private static int getPower(int x, int n){
+		
+		if(n == 0) {
+			return 1;
+		}
+		
+		int faith = getPower(x, n/2);
+		int pow = 0;
+		if(n % 2 == 0) {
+			 pow = faith * faith;
+		}else {
+			 pow = faith * faith * 2;
+		}
+		
+		return pow;
+	}
+	
+	public static void main(String args[]) {
+		Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		int n = scn.nextInt();
+		
+		int pow = getPower(x, n);
+		System.out.println(pow);
+		
+	}
+
+}
