195e8ee9fd2914e5a36ab87af2f33175ee057e6f
anujkaushik1 <66275103+anujkaushik1@users.noreply.github.com>
2022-03-28 18:16:24 +0530

    Stack To Queue Adapter - Add Efficient

---
+++ Stack_And_Queues/StackToQueueAdapterAdd.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class StackToQueueAdapter {
+    Stack<Integer> mainS;
+
+    public StackToQueueAdapter() {
+      mainS = new Stack<>();
+    }
+
+    int size() {
+        return mainS.size();
+    }
+
+    void add(int val) {
+        mainS.add(val);
+    }
+
+    int remove() {
+        if(mainS.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        Stack<Integer> helperS = new Stack<>();
+        
+        while(mainS.size() > 0){
+            int val = mainS.pop();
+            helperS.add(val);
+        }
+        
+        int val = helperS.pop();
+        
+        while(helperS.size() > 0){
+            int rval = helperS.pop();
+            mainS.add(rval);
+        }
+        
+        return val;
+    }
+
+    int peek() {
+        if(mainS.size() == 0){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+        Stack<Integer> helperS = new Stack<>();
+        
+        while(mainS.size() > 0){
+            int val = mainS.pop();
+            helperS.add(val);
+        }
+        
+        int val = helperS.peek();
+        
+        while(helperS.size() > 0){
+            int rval = helperS.pop();
+            mainS.add(rval);
+        }
+        return val;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StackToQueueAdapter qu = new StackToQueueAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(qu.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
