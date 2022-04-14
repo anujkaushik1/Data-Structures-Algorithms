91b8239214b561cba26c1d03b3273a355777412b
anujkaushik1 <66275103+anujkaushik1@users.noreply.github.com>
2022-04-14 10:46:42 +0530

    Unfold Of Linkedlist

---
+++ Linked_Lists/UnfoldOfLinkedList.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Main {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode ahead = head.next;
+        
+        while(curr != null){
+            curr.next = prev;
+            
+            prev = curr;
+            curr = ahead;
+            
+            if(ahead != null)
+                ahead = ahead.next;
+        }
+        
+        return prev;
+        
+    }
+    public static void unfold(ListNode head) {
+        ListNode l1 = head, t1 = head;
+        ListNode l2 = head.next, t2 = head.next;
+        
+        while(t1.next != null && t2.next != null){
+            t1.next = t1.next.next;
+            t2.next = t2.next.next;
+                
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        
+        l2 = reverse(l2);
+        t1.next = l2;
+    }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = dummy.next;
+        unfold(head);
+        printList(head);
+    }
+}
