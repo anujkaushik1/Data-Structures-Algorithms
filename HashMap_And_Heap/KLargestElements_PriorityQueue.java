7f655ebd07c5ffeeba17ceb435c5d96e285ef88b
anujkaushik1 <66275103+anujkaushik1@users.noreply.github.com>
2022-06-01 14:01:55 +0530

    K Largest Elements Using Min Heap

--- HashMap_And_Heap/KLargestElements_PriorityQueue.java
+++ HashMap_And_Heap/KLargestElements_PriorityQueue.java
@@ -6,18 +6,46 @@ public class Main {
     public static void approach1(int arr[], int k){
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());  // Max Heap
         
+        // TC => (N + K Log N)
+        
         // o(n logn)
         for(int val : arr) 
             pq.add(val);
         
         ArrayList<Integer> res = new ArrayList<>();
         
+        // O(K LogN)
         while(k-- > 0){
             res.add(pq.remove());
         }
         
         for(int i = res.size() - 1; i >= 0; i--){
             System.out.println(res.get(i));
+        }
+    }
+    
+    public static void approach2(int arr[], int k){
+        
+        // TC => K Log K + (N - K)*Log K = O(N Log K)
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0; i < k; i++){
+            pq.add(arr[i]);
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            int val = arr[i];
+            int top = pq.peek();
+            
+            if(val > top){
+                pq.remove();
+                pq.add(val);
+            }
+        }
+        
+        while(pq.size() > 0){
+            System.out.println(pq.remove());
         }
     }
 
@@ -32,7 +60,7 @@ public class Main {
 
       int k = Integer.parseInt(br.readLine());
       
-      approach1(arr, k);
+      approach2(arr, k);
    }
 
 }
