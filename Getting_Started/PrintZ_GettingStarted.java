9fd93250c9cf706b387eb85b73efcd2857681525
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2022-07-07 23:28:32 +0530

    Print Z

--- Getting_Started/PrintZ_GettingStarted.java
+++ Getting_Started/PrintZ_GettingStarted.java
@@ -1,5 +1,13 @@
-package Getting_Started;
+import java.util.*;
 
-public class PrintZ_GettingStarted {
+public class Main {
 
+  public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   *");
+    System.out.println("  *");
+    System.out.println(" *");
+    System.out.println("*****");
+
+  }
 }
