9bfc6dd4d406540a000370acbfd9caef068dc17d
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2022-09-25 12:00:12 +0530

    Update StockSpan.java

--- Stack_And_Queues/StockSpan.java
+++ Stack_And_Queues/StockSpan.java
@@ -1,3 +1,60 @@
+// 1st Approach
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] span = solve(a);
+    display(span);
+ }
+
+ public static int[] solve(int[] arr){
+   int ans[] = new int[arr.length];
+   
+   Stack<Integer> st = new Stack<>();
+   st.push(-1);
+   
+   for(int i = 0; i < arr.length; i++){
+       
+       int curr = arr[i];
+       
+       while(st.size() != 0 && st.peek() != -1 && curr >= arr[st.peek()]){
+           st.pop();
+       }
+       
+       if(st.size() == 0){
+           ans[i] = i + 1;
+       }
+       else{
+           ans[i] = i - st.peek();
+       }
+        st.push(i);  
+   }
+   
+   return ans;
+ }
+
+}
+
+
+// 2nd Approach
 package Stack_And_Queues;
 
 import java.io.BufferedReader;
