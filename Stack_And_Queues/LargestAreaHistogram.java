e0c2a34726f6284d9ae7dd11069c85ff828b1d03
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2022-09-25 12:13:22 +0530

    Update LargestAreaHistogram.java

--- Stack_And_Queues/LargestAreaHistogram.java
+++ Stack_And_Queues/LargestAreaHistogram.java
@@ -1,3 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    
+    private static int[] nextSmallerElement(int arr[]){
+        
+        int ans[] = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        
+        for(int i = arr.length - 1; i >= 0; i--){
+            int curr = arr[i];
+            
+            while(st.size() != 0 && st.peek() != -1 && curr <= arr[st.peek()]){
+                st.pop();
+            }
+            
+            ans[i] = st.peek();
+            st.push(i);
+            
+        }
+        return ans;
+    }
+    
+    private static int[] prevSmallerElement(int arr[]){
+         int ans[] = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        
+        for(int i = 0; i < arr.length; i++){
+            int curr = arr[i];
+            
+          while(st.size() != 0 && st.peek() != -1 && curr <= arr[st.peek()]){
+                st.pop();
+            }
+            
+            ans[i] = st.peek();
+            st.push(i);
+            
+        }
+        return ans;
+    }
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+    
+    int next[] = nextSmallerElement(a);
+    int prev[] = prevSmallerElement(a);
+    int maxArea = Integer.MIN_VALUE;
+    
+    for(int i = 0; i < a.length; i++){
+        int len = a[i];
+         if(next[i] == -1) {
+                next[i] = n;
+            }
+        int bre = next[i] - prev[i] - 1;
+        
+        int area = len * bre;
+        
+        if(area > maxArea){
+            maxArea = area;
+        }
+        
+    }
+    
+    System.out.println(maxArea);
+    
+ }
+}
+
+
+
+
+
+
 package Stack_And_Queues;
 
 import java.util.Scanner;
