d9b5db4a174ff50b99bf8402a8cd8cb7a428d48d
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2022-09-25 22:46:03 +0530

    Update CelebrityProblem.java

--- Stack_And_Queues/CelebrityProblem.java
+++ Stack_And_Queues/CelebrityProblem.java
@@ -1,85 +1,58 @@
-package Stack_And_Queues;
+import java.io.*;
+import java.util.*;
 
-import java.util.Scanner;
-import java.util.Stack;
+public class Main {
 
-public class CelebrityProblem {
-	
-	
-	private static void findCelebrity(int array[][], Stack<Integer> st) {
-		
-		while(st.size() != 1) {
-			
-			int v2 = st.pop();
-			int v1 = st.pop();
-			
-			if(array[v1][v2] == 0) {
-				st.push(v1);
-			}
-			else if(array[v1][v2] == 1) {
-				st.push(v2);
-			}
-		}
-		
-		int x = st.pop();
-		
-		if(isCelebrity(array, x)) {
-			System.out.println(x);
-		}
-		else {
-			System.out.println("none");
-		}
-		
-	}
-	
-	private static boolean isCelebrity(int array[][], int x) {
-		
-		int n = array.length;
-		
-		for(int j = 0; j < n; j++) {
-			
-			if(array[x][j] == 1) {
-				return false;
-			}
-			
-		}
-		
-		for(int i = 0; i < n; i++) {
-			
-			if(i == x) {
-				continue;
-			}
-			
-			if(array[i][x] == 0) {
-				return false;
-			}
-			
-		}
-		
-		return true;
-		
-	}
-	
-	public static void main(String args[]) {
-		
-		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
-		int array[][] = new int[n][n];
-		
-		Stack<Integer> st = new Stack<>();
-		
-		for(int i = 0; i < n; i++) {	
-			String line = scn.next();
-			
-			for(int j = 0; j < n; j++) {
-				array[i][j] = line.charAt(j) - '0';
-			}
-			
-			st.push(i);
-		}
-		
-		findCelebrity(array, st);
-		
-	}
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            String line = br.readLine();
+            for (int k = 0; k < n; k++) {
+                arr[j][k] = line.charAt(k) - '0';
+            }
+        }
+
+        findCelebrity(arr);
+
+    }
+
+    public static void findCelebrity(int[][] arr) {
+       Stack<Integer> st = new Stack<>();
+       
+       for(int i = 0; i < arr.length; i++){
+           st.push(i);
+       }
+       
+       while(st.size() != 1){
+           int v2 = st.pop();
+           int v1 = st.pop();
+           
+           if(arr[v1][v2] == 0){
+               st.push(v1);
+           }
+           else{
+               st.push(v2);
+           }
+           
+       }
+       int idx = st.pop();
+       for(int i = 0; i < arr.length;i++){
+           
+           if(i == idx){
+               continue;
+           }
+           
+           if(arr[idx][i] != 0 || arr[i][idx] != 1){
+               System.out.println("none");
+               return ;
+           }
+       }
+    System.out.print(idx);
+       
+    }
 
 }
